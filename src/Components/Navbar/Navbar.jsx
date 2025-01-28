a396e9ef451f74ba13c4513cc2d7361d25a46e17
codingcreation2-demo <ranjan162238@gmail.com>
2025-01-28 12:21:16 +0530

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import menu_icon from "../../assets/menu-icon.png";
+
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -10,19 +13,58 @@ const Navbar = () => {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     });
   }, []);
+
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  };
   return (
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} className="logo" />
-      <ul>
-        <li> Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Courses</li>
-        <li>Testimonial</li>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>
-          <button className="btn">Contact us</button>
+          {" "}
+          <Link to="hero" smooth={true} offset={0} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="program" smooth={true} offset={0} duration={500}>
+            Program
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="about" smooth={true} offset={0} duration={500}>
+            About us
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="courses" smooth={true} offset={-260} duration={500}>
+            Courses
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="testimonials" smooth={true} offset={-260} duration={500}>
+            Testimonial
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className="btn"
+          >
+            Contact us
+          </Link>
         </li>
       </ul>
+      <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
