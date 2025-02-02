10217d7ffc2d974eb2b0bb07d68b7dd099257544
codingcreation2-demo <ranjan162238@gmail.com>
2025-02-03 01:33:19 +0530

    add

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,9 +7,7 @@ const Hero = () => {
       <div className="hero-text">
         <h1>We Ensure better education for a better future</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
-          aperiam facere! Velit rem deleniti voluptatibus tenetur, reiciendis
-          aut ad non ea? Nisi nesciunt maxime et eius eos natus corrupti nam?
+          This is a Platform which you provide guidence to growth your carrier.
         </p>
         <button className="btn">
           Explore More <img src={icon} />
