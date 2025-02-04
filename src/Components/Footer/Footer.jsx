7171f6398a776cf9a4ae94bd528a447c95691d67
codingcreation2-demo <ranjan162238@gmail.com>
2025-02-04 19:09:38 +0530

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import { FaCopyright } from "react-icons/fa";
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <p>
-        {" "}
-        <FaCopyright />
-        2025 Carrier simplify. All rights reserved
-      </p>
+      <p>@ 2025 Carrier simplify. All rights reserved</p>
       <ul>
         <li>Terms & Services</li>
         <li>Privacy policy</li>
