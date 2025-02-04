cb0a52900e5c7f6d093de7f9b58663ff532234e2
codingcreation2-demo <ranjan162238@gmail.com>
2025-02-04 12:56:43 +0530

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/Carrier-simplify.png";
+
 import menu_icon from "../../assets/menu-icon.png";
 
 import { Link } from "react-scroll";
